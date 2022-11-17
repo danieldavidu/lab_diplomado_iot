4c84ef8a2cc55ccf8e7eb6c500a0cac1ad1e97a8
daniel david uribe lambis <danieldavidu@gmail.com>
2022-11-16 23:10:35 -0500

    Mandar instrucciones del modem quectel por LPUART
    se mandan instrucciones por el docklight emulando un modem quectel

--- K32L2B31A_labs_diplomado_iot/K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
+++ K32L2B31A_labs_diplomado_iot/K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
@@ -35,7 +35,7 @@ enum{
 	CMD_AT_AT_GMI_Request_Manufacturer_Identification,
 	CMD_AT_AT_GMM_Request_TA_Model_Identification,
 	CMD_AT_AT_GMR_Request_TA_Revision_Identification_of_Software_Release,
-	CMD_AT_AT_CGMI_Request_Manufacturer_Identification,
+	CMD_AT_AT_GSN_Request_International_Mobile_Equipment_Identity ,
 };
 
 enum{
@@ -58,7 +58,6 @@ typedef struct _iot_nodo_data{
 	uint16_t data_sensor_humedad;
 	//------------------------------------
 	uint16_t data_sensor_presion_atmosferica;
-	uint16_t reservado;
 	//------------------------------------
 } iot_nodo_data_t;
 
@@ -91,7 +90,7 @@ const char* cmd_at[5]={
 		"AT+GMI\r\n",
 		"AT+GMM\r\n",
 		"AT+GMR\r\n",
-		"AT+CGMI\r\n",
+		"AT+GSN\r\n",
 };
 
 uint32_t msg_size;
@@ -103,19 +102,6 @@ iot_nodo_data_t datos_locales;
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
-void ec25_print_data_raw(uint8_t *data_ptr, uint32_t data_size) {
-	for (uint32_t i = 0; i < data_size; i++) {
-		PRINTF("%c", *(data_ptr + i));
-	}
-}
-
-void ec25_print_data_ascii_hex(uint8_t *data_ptr, uint32_t data_size) {
-	for (uint32_t i = 0; i < data_size; i++) {
-		PRINTF("0x%02x ", *(data_ptr + i));
-	}
-}
-
-
 
 int main(void) {
 
@@ -127,20 +113,7 @@ int main(void) {
     /* Init FSL debug console. */
     BOARD_InitDebugConsole();
 #endif
-
-    led_off_green();
-    led_off_red();
-
-    datos_locales.data_sensor_luz_adc=Sens_dato();
-    	datos_locales.data_sensor_luz_voltaje=2;
-    	datos_locales.data_sensor_luz_lux=3;
-    	datos_locales.data_sensor_temperatura=4;
-    	datos_locales.data_sensor_humedad=5;
-    	datos_locales.data_sensor_presion_atmosferica=6;
-
-
-
-    while(1) {
+ while(1) {
     	switch (fst_estado_actual) {
     			case FSM_ESTADO_INICIO:
     				/*Escribir condiciones iniciales para le ejecución de toda la FSM*/
@@ -163,26 +136,63 @@ int main(void) {
 
     			case FSM_ESTADO_ANALIZA_NUEVO_DATO_LPUART0:
     				switch(leer_dato()){
-    				case 'l':
+    				case 'x':
     					fst_estado_actual=FSM_ESTADO_START_ADC;
     					break;
     				case 'm':
     					fst_estado_actual=FSM_ESTADO_ENVIAR_COMANDO_ATI;
     					break;
+					case 'k':
+						fst_estado_actual = FSM_ESTADO_ENVIAR_COMANDO_ATI;
+						break;
+					case 'q':
+						fst_estado_actual = FSM_ESTADO_ENVIAR_COMANDO_ATI;
+						break;
+					case 'w':
+						fst_estado_actual = FSM_ESTADO_ENVIAR_COMANDO_ATI;
+						break;
+					case 'e':
+						fst_estado_actual = FSM_ESTADO_ENVIAR_COMANDO_ATI;
+						break;
     				case 'b':
     					borrar_buffer();
+    					PRINTF("BUFFER BORRADO");
     					break;
     				default://dato ilegal
     					fst_estado_actual=FSM_ESTADO_INICIO;
+
     					break;
     				}
     				break;
 
     			case FSM_ESTADO_ENVIAR_COMANDO_ATI:
-    				PRINTF("%s",cmd_at[CMD_AT_ATI_Display_Product_Identification_Information]);
-    				fst_estado_actual=FSM_ESTADO_INICIO;
-    				break;
+					if (leer_dato()=='m'){
+						PRINTF("%s",cmd_at[CMD_AT_ATI_Display_Product_Identification_Information]);
+						fst_estado_actual=FSM_ESTADO_INICIO;
+						break;
+						}
 
+							if (leer_dato() == 'k') {
+						PRINTF("%s",cmd_at[CMD_AT_AT_GMI_Request_Manufacturer_Identification]);
+						fst_estado_actual = FSM_ESTADO_INICIO;
+						break;
+							}
+
+					if (leer_dato() == 'q') {
+						PRINTF("%s",cmd_at[CMD_AT_AT_GMM_Request_TA_Model_Identification]);
+						fst_estado_actual = FSM_ESTADO_INICIO;
+						break;
+							}
+					if (leer_dato() == 'w') {
+						PRINTF("%s",cmd_at[CMD_AT_AT_GMR_Request_TA_Revision_Identification_of_Software_Release]);
+						fst_estado_actual = FSM_ESTADO_INICIO;
+						break;
+							}
+					if (leer_dato() == 'e') {
+						PRINTF("%s",cmd_at[CMD_AT_AT_GSN_Request_International_Mobile_Equipment_Identity]);
+						fst_estado_actual = FSM_ESTADO_INICIO;
+						break;
+							}
     			case FSM_ESTADO_START_ADC:
     				/*Genera señal de START para tomar dato ADC*/
     				ADC16_SetChannelConfig(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP, &ADC0_channelsConfig[0]);
